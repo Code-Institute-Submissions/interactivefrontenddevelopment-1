17a7f099b6d7bbfba454588ae8d757b8ebb745bf
Alan Staunton <eezipc@gmail.com>
2020-05-24 11:54:21 +0000

    added more items to weather report

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,14 @@ window.addEventListener('load', ()=> {
     let weatherTemperature = document.querySelector(".h_two_weathertemperature");
     let weatherHumidity = document.querySelector(".h_two_weatherstatus");
     let weatherLocation = document.querySelector(".h_two_weatherlocation");
+    let weatherTempmax = document.querySelector(".h_two_weathertempmax");
+    let weatherTempmin = document.querySelector(".h_two_weathertempmin");
+    let weatherWind = document.querySelector(".h_two_weatherwind");
+    let weatherSunrise = document.querySelector(".h_two_weathersunrise");
+    let weatherSunset = document.querySelector(".h_two_weathersunset");
+    let weatherCountry = document.querySelector(".h_two_weathercountry");
+
+   
     
 
     if (navigator.geolocation){
@@ -21,11 +29,21 @@ window.addEventListener('load', ()=> {
                 .then(data => {
                     console.log(data);
                     const { temp, temp_max, temp_min, humidity } = data.main;
-                    const { deg, gust, speed } = data.wind;
+                    const { speed } = data.wind;
+                    const { country, sunrise, sunset } = data.sys;
+                    /*const { description, icon, main } = data.weather;*/
                     
                     weatherTemperature.textContent = temp;
+                    weatherTempmax.textContent = temp_max;
+                    weatherTempmin.textContent = temp_min
                     weatherHumidity.textContent = humidity;
+                    weatherWind.textContent = speed;
+                    weatherCountry.textContent = country;
+                    weatherSunrise.textContent = sunrise;
+                    weatherSunset.textContent = sunset;
+
                     weatherLocation.textContent = data.name;
+
                    
                 });
 
